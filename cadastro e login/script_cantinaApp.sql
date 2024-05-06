39d81b672dbbaf995be861dd5ec837ede568fe4e
Pablo Silva Franco <owonekyone591@gmail.com>
2024-05-06 16:07:32 -0300

    Deletado Cadastro e Login de Alpha
    Ação feita pois vou juntar as branches "Kowobeli-Cadastro-e-Login-PHP" e "cadastro-de-lanche" em uma pasta na Alpha

--- cadastro e login/script_cantinaApp.sql
+++
@@ -1,9 +0,0 @@
-create database cantinaApp;
-use cantinaApp;
-
-CREATE TABLE usuario (
-  `idUsuario` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `nome` varchar(60) NOT NULL,
-  `email` varchar(60) NOT NULL,
-  `senha` varchar(60) NOT NULL
-)
